e52e99ff0605bbdb4f86c9c3051ed043e0dce01a
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2011-03-01 19:03:43 +0200

    Added a views module for opinions

---
+++ opinions/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render_to_response, get_list_or_404, get_object_or_404
+from django.template import RequestContext
+from kamu.opinions.models import *
+from kamu.votes.models import Party
+
+def list_questions(request):
+	questions = Question.objects.all()
+	parties = Party.objects.all()
+
+	args = dict(questions=questions, parties=parties)
+	return render_to_response('list_questions.html', args,
+		context_instance=RequestContext(request))
