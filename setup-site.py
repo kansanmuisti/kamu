0597ddda17c9696017e482443a117b23962d32e9
Juha Yrjölä <juha.yrjola@iki.fi>
2011-03-28 00:24:06 +0300

    Add a script for initial site setup

---
+++ setup-site.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import os
+
+import settings
+
+IMAGE_PATHS = ('images/members', 'images/orgs', 'images/parties',
+               'images/users', 'images/gen')
+
+all_paths = IMAGE_PATHS
+all_paths += (settings.MEDIA_TMP_DIR, )
+
+for path in all_paths:
+    full_path = '%s%s' % (settings.MEDIA_ROOT, path)
+    try:
+        os.mkdir(full_path)
+    except OSError:
+        pass
+
