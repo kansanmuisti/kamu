1f5854e1e4420dbff2be7bbbf7960ad60d56490a
Juha Yrjölä <juha.yrjola@iki.fi>
2011-03-28 00:38:33 +0300

    CSS with asset management

--- assets.py
+++ assets.py
@@ -5,3 +5,8 @@ js = Bundle('js/jquery-1.5.1.js', 'js/jquery.cookie.js',
             'js/jquery.example.js', 'js/jquery.tooltip.js',
             'js/jquery.jnotify.js', filters='jsmin', output='gen/packed.js')
 register('js_base', js)
+
+css = Bundle(Bundle('css/kamu.css', filters='less'),
+             Bundle('css/jquery-ui-1.8.1.custom.css', filters='cssrewrite'),
+             filters='cssutils', output='gen/packed.css')
+register('css_base', css)
