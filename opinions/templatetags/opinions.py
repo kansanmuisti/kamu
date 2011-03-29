459eef10df334a4a20ba4d677abf4003f27b00cb
Jami Pekkanen <jami.pekkanen@helsinki.fi>
2011-03-30 01:57:25 +0300

    Added a promise statistics sidebar

--- opinions/templatetags/opinions.py
+++ opinions/templatetags/opinions.py
@@ -4,7 +4,9 @@ import collections
 
 from kamu.opinions.models import Question, Option, Answer, \
     VoteOptionCongruence, QuestionSessionRelevance
-from kamu.votes.models import Party, Session
+from kamu.votes.models import Party, Session, Member
+from kamu.user_voting import models as user_voting
+from django.contrib.contenttypes.models import ContentType
 from django.db import models, connection, transaction, IntegrityError
 from django.utils.translation import ugettext as _
 from django import forms
@@ -12,62 +14,30 @@ from django import forms
 from django import template
 register = template.Library()
 
-CongruenceChoice = collections.namedtuple('CongruenceChoice', 'value name')
-CONGRUENCE_CHOICES = (
-    CongruenceChoice(str(-1.0), _('Incongruent')),
-    CongruenceChoice(str(-2 / 3.0), _('Somewhat incongruent')),
-    CongruenceChoice(str(-1 / 3.0), _('Slightly incongruent')),
-    CongruenceChoice(str(0.0), _('Neutral/Irrelevant')),
-    CongruenceChoice(str(1 / 3.0), _('Slightly congruent')),
-    CongruenceChoice(str(2 / 3.0), _('Somewhat congruent')),
-    CongruenceChoice(str(1.0), _('Congruent')),
-    )
-
-
-@register.inclusion_tag('question_session_congruence_input.html',
+@register.inclusion_tag('promise_statistics_sidebar.html',
                         takes_context=True)
-def question_session_congruence_input(context, question, session):
-    request = context['request']
-    option_choices = []
-    for option in question.option_set.all():
-        name = 'congruence_%i_%i' % (session.id, option.id)
+def promise_statistics_sidebar(context, user):
+    parties = VoteOptionCongruence.objects.get_party_congruences(for_user=user)
 
-        if name in request.POST:
-            value = float(request.POST[name])
-            item = VoteOptionCongruence(option=option, session=session,
-                    congruence=value, vote='Y', user=request.user)
-            item.save(update_if_exists=True)
-            item = VoteOptionCongruence(option=option, session=session,
-                    congruence=-value, vote='N', user=request.user)
-            item.save(update_if_exists=True)
+    member_type = ContentType.objects.get_for_model(Member)
+    member_votes = user_voting.Vote.objects.filter(user=user,
+                                                   content_type=member_type)
+    members = []
+    for vote in member_votes:
+        member = Member.objects.get(pk=vote.object_id)
+        member.congruence = VoteOptionCongruence.objects.get_member_congruence(
+                                                            member,
+                                                            for_user=user)
+        if(member.congruence is None): continue
+        members.append(member)
+        
 
-        if not request.user.is_anonymous():
-            user_congruence = \
-                VoteOptionCongruence.objects.filter(option=option,
-                    session=session, vote='Y', user=request.user)
-            if user_congruence.count() > 0:
-                user_congruence = str(user_congruence[0].congruence)
-            else:
-                user_congruence = None
-        else:
-            user_congruence = None
+    return dict(parties=parties, members=members, user=user)
 
-        congruence = VoteOptionCongruence.get_congruence(option, session)
-        if congruence is None:
-            congruence = 0
-        congruence_scale = int((congruence + 1) / 2.0 * 100)
-
-        congruence_choices = CONGRUENCE_CHOICES
-        option_choices.append(dict(
-            option=option,
-            name=name,
-            congruence=congruence,
-            congruence_scale=congruence_scale,
-            congruence_choices=congruence_choices,
-            user_congruence=user_congruence,
-            ))
-
-    return dict(question=question, session=session,
-                option_choices=option_choices,
-                congruence_choices=CONGRUENCE_CHOICES, user=request.user)
-
+@register.filter
+def congruence_to_percentage(share):
+    if(share is None): return _("N/A")
+    try:
+        return "%i" % int((share+1)/2.0*100)
+    except ValueError:
+        return ''
