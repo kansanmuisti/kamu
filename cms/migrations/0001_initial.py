898d23cf3458773d2aa0e3fba82b9db8b36b59da
Ville Koivunen <ville.koivunen@kansanmuisti.fi>
2011-04-12 20:00:00 +0300

    cms: Initial south migration
    This is the initial south migration. Meaning that it will create the tables
    from scratch like syncdb would.

---
+++ cms/migrations/0001_initial.py
@@ -0,0 +1,164 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Category'
+        db.create_table('cms_category', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=80)),
+        ))
+        db.send_create_signal('cms', ['Category'])
+
+        # Adding model 'Item'
+        db.create_table('cms_item', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('category', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Category'])),
+        ))
+        db.send_create_signal('cms', ['Item'])
+
+        # Adding model 'Newsitem'
+        db.create_table('cms_newsitem', (
+            ('item_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['cms.Item'], unique=True, primary_key=True)),
+            ('date', self.gf('django.db.models.fields.DateField')()),
+        ))
+        db.send_create_signal('cms', ['Newsitem'])
+
+        # Adding model 'Content'
+        db.create_table('cms_content', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Item'])),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=10)),
+        ))
+        db.send_create_signal('cms', ['Content'])
+
+        # Adding model 'Revision'
+        db.create_table('cms_revision', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('content', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Content'])),
+            ('subject', self.gf('django.db.models.fields.CharField')(max_length=200, null=True, blank=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'], null=True, blank=True)),
+            ('data', self.gf('cms.markupfield.fields.MarkupField')(rendered_field=True)),
+            ('data_markup_type', self.gf('django.db.models.fields.CharField')(default=None, max_length=30)),
+            ('_data_rendered', self.gf('django.db.models.fields.TextField')(blank=True)),
+        ))
+        db.send_create_signal('cms', ['Revision'])
+
+        # Adding model 'Cache'
+        db.create_table('cms_cache', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('item', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Item'])),
+            ('language', self.gf('django.db.models.fields.CharField')(max_length=10)),
+            ('content', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Content'])),
+            ('revision', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['cms.Revision'])),
+        ))
+        db.send_create_signal('cms', ['Cache'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Category'
+        db.delete_table('cms_category')
+
+        # Deleting model 'Item'
+        db.delete_table('cms_item')
+
+        # Deleting model 'Newsitem'
+        db.delete_table('cms_newsitem')
+
+        # Deleting model 'Content'
+        db.delete_table('cms_content')
+
+        # Deleting model 'Revision'
+        db.delete_table('cms_revision')
+
+        # Deleting model 'Cache'
+        db.delete_table('cms_cache')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'cms.cache': {
+            'Meta': {'object_name': 'Cache'},
+            'content': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Content']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Item']"}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'revision': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Revision']"})
+        },
+        'cms.category': {
+            'Meta': {'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80'})
+        },
+        'cms.content': {
+            'Meta': {'object_name': 'Content'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Item']"}),
+            'language': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        'cms.item': {
+            'Meta': {'object_name': 'Item'},
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Category']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'cms.newsitem': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'Newsitem', '_ormbases': ['cms.Item']},
+            'date': ('django.db.models.fields.DateField', [], {}),
+            'item_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.Item']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'cms.revision': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'Revision'},
+            '_data_rendered': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'content': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['cms.Content']"}),
+            'data': ('cms.markupfield.fields.MarkupField', [], {'rendered_field': 'True'}),
+            'data_markup_type': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True', 'blank': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['cms']
