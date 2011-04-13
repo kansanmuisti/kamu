b4503644ddb6876954584e2f99b71632b35073e6
Imre Deak <imre.deak@gmail.com>
2011-04-13 08:51:24 +0300

    user voting: add get_vote and toggle_vote to the vote manager
    Needed by the upcoming patches adding support for toggling the votes.
    
    toggle_vote works like the following:
    
    old   requested  new
    ----------------------
    yes   yes        clear
    yes   no         no
    no    no         clear
    no    yes        yes
    clear clear      clear
    
    Signed-off-by: Imre Deak <imre.deak@gmail.com>

--- user_voting/managers.py
+++ user_voting/managers.py
@@ -10,6 +10,15 @@ class VoteManager(models.Manager):
                         score=score)
         return q.count()
 
+    def get_vote(self, obj, user):
+        ctype = ContentType.objects.get_for_model(obj)
+        try:
+            v = self.get(user=user, content_type=ctype,
+                         object_id=obj._get_pk_val())
+            return v.score
+        except models.ObjectDoesNotExist:
+            return None
+
     def record_vote(self, obj, user, score):
         ctype = ContentType.objects.get_for_model(obj)
         try:
@@ -24,3 +33,10 @@ class VoteManager(models.Manager):
             if score != None:
                 self.create(user=user, content_type=ctype,
                             object_id=obj._get_pk_val(), score=score)
+
+    def toggle_vote(self, obj, user, score):
+        if self.get_vote(obj, user) == score:
+            new_score = None
+        else:
+            new_score = score
+        self.record_vote(obj, user, new_score)
