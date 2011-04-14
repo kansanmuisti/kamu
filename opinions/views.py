32941526c7ccad0f266a206c31bb05d2d6d03119
Juha Yrjölä <juha.yrjola@iki.fi>
2011-04-14 16:34:38 +0300

    opinions: Fix navbar activation

--- opinions/views.py
+++ opinions/views.py
@@ -185,6 +185,7 @@ def get_promise_statistics_summary(user, question=None):
 
 def summary(request):
     args = get_promise_statistics_summary(user=request.user)
+    args['active_page'] = 'opinions'
     args['content'] = Item.objects.retrieve_content('opinions_about')
     args['no_percentages'] = True
     return render_to_response('opinions/summary.html', args,
