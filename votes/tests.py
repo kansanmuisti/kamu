kansanmuisti/kamu

commit 4b2d86751f9d36dbcf41627239418c6f262f3d0e

Add preliminary support for MP activity
Also refactor Document code.

votes/views.py
@@ -267,8 +267,8 @@ def search_by_keyword(request):
     if not 'query' in request.GET:
         raise Http404()
     kw = get_object_or_404(Keyword, name=request.GET['query'])
-    sess_ids = SessionKeyword.objects.filter(keyword=kw).values_list('session', flat=True)
-    sess_list = Session.objects.filter(id__in=sess_ids).order_by('-plenary_session__date')
+    sess_ids = kw.session_set.all().values_list('id', flat=True)
+    sess_list = Session.objects.filter(id__in=sess_ids).order_by('-plenary_session__date', 'number')
 
     args = {'sess_list': sess_list, 'keyword': kw}
 
@@ -366,9 +366,9 @@ def show_session_basic(request, session, psess):
         query = Vote.objects.all()
     query &= query.filter(session = session).order_by(order, 'member__name')
     votes = query.select_related('member', 'member__party')
-    session.docs = SessionDocument.objects.filter(sessions=session)
-    session.kw_list = session.sessionkeyword_set.values_list('keyword__name', flat=True).order_by('keyword__name')
-    for doc in session.docs:
+    session.kw_list = session.keywords.values_list('name', flat=True).order_by('name')
+    session.doc_list = session.docs.order_by('sessiondocument__order')
+    for doc in session.doc_list:
         if doc.summary:
             session.summary = doc.summary.replace('\n', '\n\n')
             break
@@ -894,20 +894,19 @@ def autocomplete_search(request):
 
     members = members[:max_results]
 
-    keywords = word_search(SessionKeyword, 'keyword__name', words,
-                           trailing_space)
+    """keywords = word_search(Keyword, 'name', words, trailing_space)
     if keywords is None:
         return HttpResponseBadRequest()
-    keywords = keywords.values('keyword__name').distinct()[:max_results]
+    keywords = keywords.values('name').distinct()[:max_results]"""
 
     result_list = []
     for x in members:
         tn = DjangoThumbnail(x.photo, (thumbnail_width, thumbnail_height))
         result_list.append((x.name, unicode(tn), x.get_absolute_url()))
 
-    for x in keywords:
+    """for x in keywords:
         result_list.append((x['keyword__name'], "",
-                            "/search/keyword/?query=" + x['keyword__name']))
+                            "/search/keyword/?query=" + x['keyword__name']))"""
 
     result_list.sort(key=lambda n:n[0])
     result_list = result_list[:max_results]
@@ -968,8 +967,8 @@ def tagcloud_keywords(search_depth, max_keywords):
     sess_list = Session.objects.filter(plenary_session__in=pls)
     sess_list = sess_list.order_by('plenary_session__date')
 
-    skw_list = SessionKeyword.objects.              \
-                filter(session__in=sess_list).      \
+    mgr = Session.keywords.through.objects
+    skw_list = mgr.filter(session__in=sess_list).   \
                 values('keyword').                  \
                 annotate(count=Count('keyword')).   \
                 values('keyword__name', 'count').   \
@@ -981,7 +980,7 @@ def tagcloud_keywords(search_depth, max_keywords):
             'name':k['keyword__name'],
             'url':'/search/keyword/?query=' + k['keyword__name'],
             'count':k['count'],
-        };
+        }
         kw_list.append(kw)
 
     kw_list.sort(key=lambda n:n['name'])
@@ -1041,7 +1040,8 @@ def about(request, section):
 	section_name = _('Feedback')
     else:
         raise Http404
-    sess_list = Session.objects.all().order_by('-plenary_session__date', '-number')
+    sess_list = Session.objects.all().order_by('-plenary_session__date',
+                '-plenary_session__name', '-number')
     sess_list = sess_list.select_related('plenary_session')[:5]
     for ses in sess_list:
         ses.count_votes()

templates/show_session.html
@@ -86,8 +86,8 @@
   <div id='session_doc_list'>
     <ul>
       <li><a class="action_link" href="{{ psession.info_link }}">{% trans "Plenary session minutes" %}</a>
-{% for doc in session.docs %}
-      <li><a class='action_link' href='{{ doc.info_link }}'>{{ doc.name }}</a></li>
+{% for doc in session.doc_list %}
+      <li><a class='action_link' href='{{ doc.info_link }}'>{{ doc.type }} {{ doc.name }}</a></li>
 {% endfor %}
     </ul>
   </div>

utils/vote_list_parser.py
@@ -138,10 +138,12 @@ class Parser(SGMLParser):
                 if not self.in_docs == 2:
                         return
                 self.info = self.info.rstrip()
-                m = re.match(r'\w+ \d+/\d{4}', self.info)
+                m = re.match(r'(\w+) (\d+/\d{4})', self.info)
                 if not m:
                         raise Exception("Invalid document name")
-                self.desc['docs'].append((m.group(0), self.doc_link))
+                doc = {'type': m.groups()[0], 'id': m.groups()[1]}
+                doc['url'] = self.doc_link
+                self.desc['docs'].append(doc)
                 self.in_docs = 1
 
         def start_tbody(self, attrs):

votes/migrations/0007_auto__del_sessiondocument.py
@@ -1,0 +1,170 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Deleting model 'SessionDocument'
+        db.delete_table('votes_sessiondocument')
+
+        # Removing M2M table for field sessions on 'SessionDocument'
+        db.delete_table('votes_sessiondocument_sessions')
+    
+    
+    def backwards(self, orm):
+        
+        # Adding model 'SessionDocument'
+        db.create_table('votes_sessiondocument', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('url_name', self.gf('django.db.models.fields.SlugField')(max_length=20, unique=True, db_index=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=20, unique=True, db_index=True)),
+            ('info_link', self.gf('django.db.models.fields.URLField')(max_length=200, null=True, blank=True)),
+            ('summary', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('votes', ['SessionDocument'])
+
+        # Adding M2M table for field sessions on 'SessionDocument'
+        db.create_table('votes_sessiondocument_sessions', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('sessiondocument', models.ForeignKey(orm['votes.sessiondocument'], null=False)),
+            ('session', models.ForeignKey(orm['votes.session'], null=False))
+        ))
+        db.create_unique('votes_sessiondocument_sessions', ['sessiondocument_id', 'session_id'])
+    
+    
+    models = {
+        'votes.county': {
+            'Meta': {'object_name': 'County'},
+            'district': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'votes.districtassociation': {
+            'Meta': {'object_name': 'DistrictAssociation'},
+            'begin': ('django.db.models.fields.DateField', [], {}),
+            'end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Member']"}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'votes.keyword': {
+            'Meta': {'object_name': 'Keyword'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128', 'db_index': 'True'})
+        },
+        'votes.member': {
+            'Meta': {'object_name': 'Member'},
+            'birth_date': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75'}),
+            'given_names': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'homepage_link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info_link': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Party']", 'null': 'True', 'blank': 'True'}),
+            'phone': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'surname': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'twitter_account': ('django.db.models.fields.CharField', [], {'max_length': '30', 'null': 'True', 'blank': 'True'}),
+            'url_name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '50', 'db_index': 'True'}),
+            'wikipedia_link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'votes.memberstats': {
+            'Meta': {'object_name': 'MemberStats'},
+            'begin': ('django.db.models.fields.DateField', [], {}),
+            'election_budget': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Member']"}),
+            'party_agreement': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '20'}),
+            'session_agreement': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '20'}),
+            'statement_count': ('django.db.models.fields.IntegerField', [], {}),
+            'vote_counts': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '30'})
+        },
+        'votes.minutes': {
+            'Meta': {'object_name': 'Minutes'},
+            'html': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'plenary_session': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.PlenarySession']", 'unique': 'True'})
+        },
+        'votes.party': {
+            'Meta': {'object_name': 'Party'},
+            'full_name': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'info_link': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'logo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '10', 'primary_key': 'True'})
+        },
+        'votes.partyassociation': {
+            'Meta': {'object_name': 'PartyAssociation'},
+            'begin': ('django.db.models.fields.DateField', [], {}),
+            'end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Member']"}),
+            'party': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Party']"})
+        },
+        'votes.plenarysession': {
+            'Meta': {'object_name': 'PlenarySession'},
+            'date': ('django.db.models.fields.DateField', [], {'db_index': 'True'}),
+            'info_link': ('django.db.models.fields.URLField', [], {'max_length': '200'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '20', 'primary_key': 'True'}),
+            'term': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Term']"}),
+            'url_name': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '20', 'db_index': 'True'})
+        },
+        'votes.session': {
+            'Meta': {'object_name': 'Session'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'info': ('django.db.models.fields.TextField', [], {}),
+            'info_link': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'}),
+            'number': ('django.db.models.fields.IntegerField', [], {}),
+            'plenary_session': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.PlenarySession']"}),
+            'subject': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'time': ('django.db.models.fields.TimeField', [], {}),
+            'vote_counts': ('django.db.models.fields.CommaSeparatedIntegerField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'})
+        },
+        'votes.sessionkeyword': {
+            'Meta': {'object_name': 'SessionKeyword'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Keyword']"}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Session']"})
+        },
+        'votes.statement': {
+            'Meta': {'unique_together': "(('plenary_session', 'dsc_number', 'index'),)", 'object_name': 'Statement'},
+            'dsc_number': ('django.db.models.fields.IntegerField', [], {}),
+            'html': ('django.db.models.fields.TextField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'index': ('django.db.models.fields.IntegerField', [], {}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Member']", 'null': 'True', 'blank': 'True'}),
+            'plenary_session': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.PlenarySession']"}),
+            'text': ('django.db.models.fields.TextField', [], {})
+        },
+        'votes.term': {
+            'Meta': {'object_name': 'Term'},
+            'begin': ('django.db.models.fields.DateField', [], {}),
+            'display_name': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'end': ('django.db.models.fields.DateField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '40'}),
+            'visible': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'})
+        },
+        'votes.termmember': {
+            'Meta': {'object_name': 'TermMember'},
+            'election_budget': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '2', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Member']"}),
+            'term': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Term']"})
+        },
+        'votes.vote': {
+            'Meta': {'object_name': 'Vote'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'member': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Member']"}),
+            'party': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'session': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['votes.Session']"}),
+            'vote': ('django.db.models.fields.CharField', [], {'max_length': '1', 'db_index': 'True'})
+        }
+    }
+    
+    complete_apps = ['votes']

commit 9c0ed4c023dbfc8c0765708d6c046075f228ee04

autocomplete: fixups for the Document code refactoring
The keyword autocomplete python backend and test cases got
broken by:
4b2d86751f - Add preliminary support for MP activity

Fix things up now accordingly.

Signed-off-by: Imre Deak <imre.deak@gmail.com>

votes/tests.py
@@ -2,6 +2,7 @@
 
 from django.test import TestCase
 from django.core.urlresolvers import reverse
+from django.template.defaultfilters import slugify
 import json
 
 class AutocompleteTestCase(TestCase):
@@ -32,7 +33,7 @@ class AutocompleteTestCase(TestCase):
         self.assertEqual(decoded_res, exp_res, msg=err_msg)
 
 class AutocompleteSearchTest(AutocompleteTestCase):
-    fixtures = ['test_keyword', 'test_session_keyword', 'test_member']
+    fixtures = ['test_keyword', 'test_session', 'test_member']
     req_url = reverse('votes.views.autocomplete_search')
     req_params = {
             u'max_results'      : 6,
@@ -42,35 +43,41 @@ class AutocompleteSearchTest(AutocompleteTestCase):
 
     def setUp(self):
         qpar_str = u'?query='
-        search_kw_url = reverse('votes.views.search_by_keyword') + qpar_str
-        search_mb_url = reverse('votes.views.search') + qpar_str
+
+        def kw_url_res(kw):
+            return reverse(u'votes.views.search_by_keyword') + u'?query=' + kw
+
+        def mb_url_res(mb):
+            return reverse(u'votes.views.show_member',
+                           kwargs={'member':slugify(mb)})
 
         def query_res(name, exp_url):
             return {
                 'name'      : name,
-                'url'       : exp_url + name,
+                'url'       : exp_url,
             }
 
         def kw_res(name):
-            return query_res(name, search_kw_url)
+            return query_res(name, kw_url_res(name))
 
         def mb_res(name):
-            return query_res(name, search_mb_url)
-
-        def query_req(term, exp_url, exp_names):
-            if type(exp_names) is not list:
-                exp_names = [exp_names]
-
-            return {
-                'term'    : term,
-                'exp_res' : [query_res(n, exp_url) for n in exp_names],
-            }
+            return query_res(name, mb_url_res(name))
 
         def kw_req(term, exp_names):
-            return query_req(term, search_kw_url, exp_names)
+            if type(exp_names) is not list:
+                exp_names = [exp_names]
+            return {
+                'term'    : term,
+                'exp_res' : [query_res(n, kw_url_res(n)) for n in exp_names]
+            }
 
         def mb_req(term, exp_names):
-            return query_req(term, search_mb_url, exp_names)
+            if type(exp_names) is not list:
+                exp_names = [exp_names]
+            return {
+                'term'    : term,
+                'exp_res' : [query_res(n, mb_url_res(n)) for n in exp_names]
+            }
 
         def mix_req(term, exp_results):
             return {
@@ -98,8 +105,8 @@ class AutocompleteSearchTest(AutocompleteTestCase):
                                                   u'Ahonen Esko']),
 
                 # keyword queries returning multiple results
-                kw_req('af',                   [u'Afganistan',
-                                                  u'Afrikka']),
+                kw_req('f',                    [u'Finanssivalvonta',
+                                                  u'Fortum']),
 
                 # mixed queries returning both members and keywords
                 mix_req(u'aa',               [mb_res(u'Aaltonen Markus'),
@@ -110,9 +117,9 @@ class AutocompleteSearchTest(AutocompleteTestCase):
                 mix_req(u'',                 [mb_res(u'Aaltonen Markus'),
                                                   kw_res(u'Aasia'),
                                                   kw_res(u'Adoptio'),
-                                                  kw_res(u'Afganistan'),
                                                   kw_res(u'Afrikka'),
-                                                  mb_res(u'Ahde Matti')]),
+                                                  mb_res(u'Ahde Matti'),
+                                                  mb_res(u'Aho Esko')]),
 
                 # query returning nothing
                 mb_req(u'aa ',                []),

votes/views.py
@@ -852,13 +852,13 @@ def get_req_param_int(request, param):
         val = 0
     return val
 
-def word_search(data_set, key_name, words, trailing_space):
+def word_search(query_set, key_name, words, trailing_space):
     full_word_cnt = len(words)
 
     if full_word_cnt > 10:
         return None
     if full_word_cnt == 0:
-        return data_set.objects.all().order_by(key_name)
+        return query_set.all().order_by(key_name)
 
     query = Q()
 
@@ -873,7 +873,7 @@ def word_search(data_set, key_name, words, trailing_space):
         re = r'(^| )' + words[-1]
         query &= Q(**{key_name + '__iregex':re})
 
-    return data_set.objects.filter(query).order_by(key_name)
+    return query_set.filter(query).order_by(key_name)
 
 def autocomplete_search(request):
     name = request.GET.get('name', '')
@@ -888,25 +888,26 @@ def autocomplete_search(request):
 
     words = name.split()
     trailing_space = name and name[-1] == ' '
-    members = word_search(Member, 'name', words, trailing_space)
+    members = word_search(Member.objects, 'name', words, trailing_space)
     if members is None:
         return HttpResponseBadRequest()
 
     members = members[:max_results]
 
-    """keywords = word_search(Keyword, 'name', words, trailing_space)
+    keywords = Keyword.objects.filter(session__isnull=False)
+    keywords = word_search(keywords, 'name', words, trailing_space)
     if keywords is None:
         return HttpResponseBadRequest()
-    keywords = keywords.values('name').distinct()[:max_results]"""
+    keywords = keywords.values('name').distinct()[:max_results]
 
     result_list = []
     for x in members:
         tn = DjangoThumbnail(x.photo, (thumbnail_width, thumbnail_height))
         result_list.append((x.name, unicode(tn), x.get_absolute_url()))
 
-    """for x in keywords:
-        result_list.append((x['keyword__name'], "",
-                            "/search/keyword/?query=" + x['keyword__name']))"""
+    for x in keywords:
+        result_list.append((x['name'], "",
+                            "/search/keyword/?query=" + x['name']))
 
     result_list.sort(key=lambda n:n[0])
     result_list = result_list[:max_results]
@@ -923,7 +924,7 @@ def autocomplete_county(request):
         return HttpResponseBadRequest()
 
     trailing_space = name and name[-1] == ' '
-    counties = word_search(County, 'name', name.split(), trailing_space)
+    counties = word_search(County.objects, 'name', name.split(), trailing_space)
     if counties is None:
         return HttpResponseBadRequest()
     counties = counties[:max_results]