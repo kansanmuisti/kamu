cc7213d6eea273b6311b0259bfae282338af8073
Juha Yrjölä <juha.yrjola@iki.fi>
2012-02-04 18:19:23 +0200

    Eduskunta: Fix sgm-to-xml.sh invocation

--- eduskunta/sgml-to-xml.sh
+++ eduskunta/sgml-to-xml.sh
@@ -4,7 +4,10 @@ CATALOG_FILE="eduskunta.cat"
 INPUT="$1"
 OUTPUT="$2"
 
+MY_PATH="$(dirname "$0")"
+CATALOG_FILE="$MY_PATH"/$CATALOG_FILE
+
 set -e
 
-osx -xno-nl-in-tag -xpreserve-case -c eduskunta.cat "$INPUT" > "$OUTPUT"
+osx -xno-nl-in-tag -xpreserve-case -c "$CATALOG_FILE" "$INPUT" > "$OUTPUT"
 tidy -q -utf8 -xml -m -i $OUTPUT
