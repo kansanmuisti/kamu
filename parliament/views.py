400b845196f7123349f1b692817927e310d89699
Juha Yrjölä <juha.yrjola@iki.fi>
2012-02-22 00:06:08 +0200

    Implement AJAX-based read-more for social media updates

--- parliament/views.py
+++ parliament/views.py
@@ -24,6 +24,18 @@ def show_item(request, plsess, item_nr, subitem_nr=None):
 def show_member(request, member):
     pass
 
+def _get_parliament_activity(request, offset):
+    q = Q(nr_votes__gt=0) | Q(nr_statements__gt=0)
+    pl_items = list(PlenarySessionItem.objects.filter(q).select_related('plsess')[offset:offset+5])
+    for item in pl_items:
+        if item.nr_statements:
+            st = item.statement_set.all()[0]
+            item.statement = st
+    act_html = render_to_string('parliament/_plitem_list.html', {'items': pl_items},
+                                context_instance=RequestContext(request))
+
+    return {'offset': offset + 5, 'html': act_html}
+
 def get_parliament_activity(request):
     offset = request.GET.get('offset', 0)
     try:
@@ -33,24 +45,14 @@ def get_parliament_activity(request):
     if offset < 0:
         raise Http400()
 
-    q = Q(nr_votes__gt=0) | Q(nr_statements__gt=0)
-    pl_items = list(PlenarySessionItem.objects.filter(q).select_related('plsess')[offset:offset+5])
-    for item in pl_items:
-        if item.nr_statements:
-            st = item.statement_set.all()[0]
-            item.statement = st
-    act_html = render_to_string('parliament/ajax_activity.html', {'items': pl_items},
-                                context_instance=RequestContext(request))
-
-    data = {'offset': offset + 5, 'html': act_html}
+    data = _get_parliament_activity(request, offset)
     return HttpResponse(simplejson.dumps(data), mimetype="application/json")
 
-def main(request):
-    args = {}
+def _get_mp_some_activity(request, offset):
     q = Update.objects.filter(feed__in=MemberSocialFeed.objects.all())
-    some_updates = q.select_related('feed__membersocialfeed')[0:5]
-    update_list = []
-    for upd in some_updates:
+    some_query = q.select_related('feed__membersocialfeed')[offset:offset+5]
+    some_updates = []
+    for upd in some_query:
         d = {}
         feed = upd.feed.membersocialfeed
         mp = feed.member
@@ -62,16 +64,34 @@ def main(request):
         d['mp_portrait'] = tn.url
         d['mp_link'] = mp.get_absolute_url()
         d['mp_party'] = mp.party.name
-        update_list.append(d)
-    args['some_updates'] = update_list
+        some_updates.append(d)
+    some_html = render_to_string('parliament/_some_update_list.html',
+                                 {'some_updates': some_updates},
+                                 context_instance=RequestContext(request))
+    return {'offset': offset + 5, 'html': some_html}
 
-    q = Q(nr_votes__gt=0) | Q(nr_statements__gt=0)
-    pl_items = list(PlenarySessionItem.objects.filter(q).select_related('plsess')[0:5])
-    for item in pl_items:
-        if item.nr_statements:
-            st = item.statement_set.all()[0]
-            item.statement = st
-    args['pl_items'] = pl_items
+def get_mp_some_activity(request):
+    offset = request.GET.get('offset', 0)
+    try:
+        offset = int(offset)
+    except TypeError:
+        raise Http400()
+    if offset < 0:
+        raise Http400()
+
+    data = _get_mp_some_activity(request, offset)
+    return HttpResponse(simplejson.dumps(data), mimetype="application/json")
+
+def main(request):
+    args = {}
+
+    some_data = _get_mp_some_activity(request, 0)
+    args['some_html'] = some_data['html']
+    args['some_offset'] = some_data['offset']
+
+    parl_data = _get_parliament_activity(request, 0)
+    args['parl_act_html'] = parl_data['html']
+    args['parl_act_offset'] = parl_data['offset']
 
     return render_to_response('new_main.html', args,
                               context_instance=RequestContext(request))
