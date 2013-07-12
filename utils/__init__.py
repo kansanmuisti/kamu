8ffaeda7d9be151e20aef9c06518574c7c7a6727
Juha Yrjölä <juha.yrjola@iki.fi>
2013-07-12 20:59:24 +0300

    Add decorator for timing functions

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+import time
+
+def time_func(f):
+    def wrap(*args, **kwargs):
+        time1 = time.time()
+        ret = f(*args, **kwargs)
+        time2 = time.time()
+        print '%s function took %0.3f ms' % (f.func_name, (time2-time1)*1000.0)
+        return ret
+    return wrap
