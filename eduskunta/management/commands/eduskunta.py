bed6fa8bd7d444a40274803948d3518721ea729f
Juha Yrjölä <juha.yrjola@iki.fi>
2014-02-01 12:42:36 +0200

    Eduskunta importer: rename update to replace

--- eduskunta/management/commands/eduskunta.py
+++ eduskunta/management/commands/eduskunta.py
@@ -12,7 +12,6 @@ from ...doc import DocImporter
 from utils.http import HttpFetcher
 
 class Command(BaseCommand):
-    #args = '<poll_id poll_id ...>'
     help = 'Import data from the Finnish parliament'
     option_list = BaseCommand.option_list + (
         make_option('--party', action='store_true', dest='party',
@@ -32,8 +31,8 @@ class Command(BaseCommand):
         make_option('--single', metavar='ID', dest='single', help='Import only a single element'),
         make_option('--from-year', metavar='YEAR', dest='from_year', help='Start importing from YEAR'),
         make_option('--from-id', metavar='ID', dest='from_id', help='Start importing from ID'),
-        make_option('--update', action='store_true', dest='update',
-                    default=False, help='Update values of existing objects'),
+        make_option('--replace', action='store_true', dest='replace',
+                    default=False, help='Replace values of existing objects'),
         make_option('--massive', action='store_true', dest='massive',
                     default=False, help='Optimize for large updates'),
         make_option('--refresh', action='store_true', dest='refresh',
@@ -47,18 +46,18 @@ class Command(BaseCommand):
         if options['cache']:
             http.set_cache_dir(options['cache'])
         min_importer = MinutesImporter(http_fetcher=http)
-        min_importer.replace = options['update']
+        min_importer.replace = options['replace']
         min_importer.import_terms()
 
         if options['party']:
             importer = PartyImporter(http_fetcher=http)
-            importer.replace = options['update']
+            importer.replace = options['replace']
             importer.import_parties()
             importer.import_governments()
             importer.import_governingparties()
         if options['member']:
             importer = MemberImporter(http_fetcher=http)
-            importer.replace = options['update']
+            importer.replace = options['replace']
             importer.import_districts()
             args = {}
             if options['single']:
@@ -66,7 +65,7 @@ class Command(BaseCommand):
             importer.import_members(args)
         if options['seat']:
             importer = SeatImporter(http_fetcher=http)
-            importer.replace = options['update']
+            importer.replace = options['replace']
             importer.import_seats()
         if options['minutes']:
             args = {}
@@ -80,7 +79,7 @@ class Command(BaseCommand):
             min_importer.import_minutes(args)
         if options['docs']:
             importer = DocImporter(http_fetcher=http)
-            importer.replace = options['update']
+            importer.replace = options['replace']
             args = {}
             if options['single']:
                 args['single'] = options['single']
@@ -93,9 +92,9 @@ class Command(BaseCommand):
                 importer.import_docs(**args)
         if options['vote']:
             importer = VoteImporter(http_fetcher=http)
-            importer.replace = options['update']
+            importer.replace = options['replace']
             importer.import_votes()
         if options['funding']:
             importer = FundingImporter(http_fetcher=http)
-            importer.replace = options['update']
+            importer.replace = options['replace']
             importer.import_funding()
