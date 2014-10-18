3cfa4f48c6bf28ed4273004d9a44173ecb4b195c
Juha Yrjölä <juha.yrjola@iki.fi>
2014-10-18 20:08:19 +0300

    Allow governing templatetag to be called with a Statement object

--- parliament/templatetags/parliament.py
+++ parliament/templatetags/parliament.py
@@ -1,8 +1,19 @@
 from django import template
+from ..models import Party, Statement
 
 register = template.Library()
 
 @register.filter(name='governing')
-def governing(party, date):
-    return party.is_governing(date)
-
+def governing(obj, date=None):
+    if isinstance(obj, Party):
+        assert date is not None, "Date must be supplied when 'govern' is called with a Party object"
+        return obj.is_governing(date)
+    elif isinstance(obj, Statement):
+        if obj.member is None:
+            if 'ministeri' in obj.speaker_role:
+                return True
+            else:
+                return False
+        if date is None:
+            date = obj.item.plsess.date
+        return obj.member.party.is_governing(date)
